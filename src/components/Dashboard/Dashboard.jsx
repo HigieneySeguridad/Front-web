bec50a9c033e5c86f777110fcd1ba92cba9c9a9d
Nahuel Paniagua <nahu.1999@outlook.com>
2023-11-08 16:39:12 -0300

    fix graficos dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,44 +1,66 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Chart } from 'chart.js'
+import axios from "axios"
 import "./dashboard.css"
 import { Aside } from '../Aside'
 
 export const Dashboard = () => {
+  const [chartData, setChartData] = useState({
+    labels: ['Protecciones', 'Peligros', 'Riegos', 'Medidas'],
+    datasets: [
+      {
+        label: 'Línea 1',
+        data: [],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      },
+      {
+        label: 'Suma de "true"',
+        data: [], // Aquí guardaremos la suma de "true"
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: 'red', // Color para la línea de la suma
+        borderWidth: 4,
+        pointBackgroundColor: 'red'
+      }
+    ]
+  });
+
   useEffect(() => {
+    // Realiza una solicitud GET para obtener los datos del servidor
+    axios.get('http://localhost:3000/formularios/proteccion') // Asegúrate de que la ruta coincida con la definida en tu servidor
+      .then((response) => {
+        const newData = response.data; // Los datos obtenidos del servidor
+
+        // Calcula la suma de "true" y actualiza el estado del componente
+        const sumaTrue = calcularSumaDeTrue(newData);
+        setChartData((prevChartData) => {
+          // Actualiza los datos en los conjuntos de datos, incluyendo la suma de "true"
+          const newDatasets = prevChartData.datasets.map((dataset, index) => ({
+            ...dataset,
+            data: index === 1 ? [sumaTrue] : newData[index], // Actualiza el conjunto de datos de la suma
+          }));
+          return { ...prevChartData, datasets: newDatasets };
+        });
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos del servidor', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Inicializa o actualiza el gráfico cuando cambien los datos
     const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
       type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
+      data: chartData,
       options: {
         plugins: {
           legend: {
-            display: false
+            display: true
           },
           tooltip: {
             boxPadding: 3
@@ -46,11 +68,27 @@ export const Dashboard = () => {
         }
       }
     });
+
     return () => {
       myChart.destroy();
     };
-  }, [])
+  }, [chartData]);
 
+  // Función para calcular la suma de "true" en los valores de los checkboxes
+  const calcularSumaDeTrue = (datos) => {
+    let suma = 0;
+    datos.forEach((item) => {
+      if (item.checkboxes) {
+        Object.values(item.checkboxes).forEach((valor) => {
+          if (valor === 'true') {
+            suma++;
+          }
+        });
+      }
+    });
+    return suma;
+  };
+   
   return (
 <div className="container-fluid">
   <div className="row">
@@ -58,16 +96,6 @@ export const Dashboard = () => {
     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h2 style={{color: 'black'}}>Gráficos</h2>
-        <div className="btn-toolbar mb-2 mb-md-0">
-          <div className="btn-group me-2">
-            <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-            <svg className="bi"><use xlinkHref="#calendar3"/></svg>
-            This week
-          </button>
-        </div>
       </div>
 
       <canvas className="my-4 w-100" id="myChart" width="900" height="380"></canvas>
